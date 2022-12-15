8fb10ba88bdc04425f7faa2fde04ddb27a997c14
Chazz <czcarrizales@yahoo.com>
2022-12-14 17:09:45 -0800

    finally was able to delete a created message after a certain amount of time. DO NOT FORGOT TO RESTART YOUR SERVER WHEN YOU MAKE CHANGES. OR, YOU CAN JUST USE NODEMAN. I REALLY SUGGEST USING NODEMAN, SO YOU'RE NOT STUCK FOR AN HOUR TRYING TO FIGURE OUT WHY CHANGES ARE NOT COMING IN.

--- backend/models/letterModel.js
+++ backend/models/letterModel.js
@@ -2,9 +2,12 @@ const mongoose = require('mongoose')
 const {Schema} = mongoose;
 
 const letterSchema = new Schema({
-    message: String
+    message: String,
+    createdAt: {type: Date, default: Date.now}
 })
+
+letterSchema.index({createdAt: 1}, {expireAfterSeconds: 30})
 
 const Letter = mongoose.model('Letter', letterSchema)
 
-module.exports = Letter;
+module.exports = Letter
