2afc26114117dce4ece1e3046261423ea5467675
Chazz <czcarrizales@yahoo.com>
2022-12-14 18:19:26 -0800

    create state for letter, create function to get a random letter from the mongodb server, and then displaying that on the home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import './App.css';
 import Navbar from './Navbar'
+import axios from 'axios'
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [letter, setLetter] = useState('no message')
+
+  useEffect(() => {
+    getMessage()
+    console.log('effect')
+  }, [])
+
+  function getMessage() {
+    axios.get('http://localhost:5000')
+      .then((response) => {
+        const randomMessage = response.data[Math.floor(Math.random() * response.data.length)].message
+        setLetter(randomMessage)
+        console.log('message recieved!')
+        
+      })
+      .catch(console.log('OH NOOOOOO'))
+  }
+
   return (
     <div className="App">
       <Navbar />
+      <div className='letter-container'>
+        {letter}
+        <button onClick={getMessage}>See New Letter</button>
+      </div>
     </div>
   );
 }
