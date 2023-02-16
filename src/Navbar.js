80c347dacdf9503e7e20fe6723502e8e12439257
Chazz <czcarrizales@yahoo.com>
2023-02-15 16:22:35 -0800

    creating Links for Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,12 @@
 import './Navbar.css';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
     <div className='nav'>
       Burning Letters
+      <Link to={'/create-letter'}>Create</Link>
+      <Link to={'/view-letter'}>View</Link>
     </div>
   );
 }
