4018042dec41fd58bbd14168358fd905f0df441f
Chazz <czcarrizales@yahoo.com>
2023-02-15 16:23:09 -0800

    adding function to getMessage to display on ViewLetter.js

---
+++ src/ViewLetter.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import "./ViewLetter.css";
+
+function ViewLetter() {
+
+  const [letter, setLetter] = useState("no message");
+
+  useEffect(() => {
+    getMessage();
+    console.log("effect");
+  }, []);
+
+  function getMessage() {
+    axios
+      .get("http://localhost:5000")
+      .then((response) => {
+        const randomMessage =
+          response.data[Math.floor(Math.random() * response.data.length)]
+            .message;
+        setLetter(randomMessage);
+        console.log("message recieved!");
+      })
+      .catch(console.log("OH NOOOOOO"));
+  }
+
+  return (
+    <div className="view-letter-container">{letter}</div>
+  );
+}
+
+export default ViewLetter;
