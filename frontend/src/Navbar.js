6717c52df3ccf7d98942413193344a1e1d3f613d
Chazz <czcarrizales@yahoo.com>
2023-02-16 15:20:05 -0800

    adding active nav links for navbar

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,12 +1,12 @@
 import './Navbar.css';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 function Navbar() {
   return (
     <div className='nav'>
-      <Link to={'/'}>Burning Letters</Link>
-      <Link to={'/create-letter'}>Create</Link>
-      <Link to={'/view-letter'}>View</Link>
+      <NavLink to={'/'}>Burning Letters</NavLink>
+      <NavLink to={'/create-letter'}>Create</NavLink>
+      <NavLink to={'/view-letter'}>View</NavLink>
     </div>
   );
 }
