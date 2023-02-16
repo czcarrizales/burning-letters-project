a63c383058874f93fda6b8deeb5455d89aebfc62
Chazz <czcarrizales@yahoo.com>
2023-02-15 18:51:27 -0800

    adding loading screen when retrieving a new letter, and not allowing repeats of the same letter index

--- src/ViewLetter.js
+++ src/ViewLetter.js
@@ -3,32 +3,45 @@ import { useState, useEffect } from "react";
 import "./ViewLetter.css";
 
 function ViewLetter() {
-
   const [letter, setLetter] = useState("no message");
+  const [letterIndex, setLetterIndex] = useState();
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     getMessage();
     console.log("effect");
   }, []);
 
+  function randomNumber(length) {
+    return Math.floor(Math.random() * length);
+  }
+
   function getMessage() {
+    setLoading(true)
     axios
       .get("http://localhost:5000")
       .then((response) => {
-        const randomMessage =
-          response.data[Math.floor(Math.random() * response.data.length)]
-            .message;
-        setLetter(randomMessage);
-        console.log("message recieved!");
+        const index = randomNumber(response.data.length);
+        console.log(index)
+        console.log(letterIndex)
+        if (index !== letterIndex) {
+          console.log("it is not the same");
+          setLetterIndex(index);
+          const randomMessage = response.data[letterIndex].message;
+          setLetter(randomMessage);
+          setLoading(false)
+        } else {
+            getMessage()
+        }
       })
       .catch(console.log("OH NOOOOOO"));
   }
 
   return (
     <div className="view-letter-container">
-        <p>{letter}</p>
-        <button onClick={getMessage}>View New Letter</button>
-        </div>
+      <p>{loading ? 'loading...' : letter}</p>
+      <button onClick={getMessage}>View New Letter</button>
+    </div>
   );
 }
 
